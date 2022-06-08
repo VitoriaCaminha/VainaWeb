07621398979c44a74a9640bdd21a5dd222135177
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-08 20:57:18 -0300

    First class of module 2

---
+++ module2/class1/script.js
@@ -0,0 +1,12 @@
+let name = "John Doe"
+let number = 85
+
+let year = 2022 - number
+console.log(year)
+console.log(number + year)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
