79e3887acee3e4f2a25653f54c0e7ae5d179aaf6
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-09 12:41:46 -0300

    First class of module 2

--- module2/class1/script.js
+++ module2/class1/script.js
@@ -1,12 +1,61 @@
-let name = "John Doe"
-let number = 85
+//Ordem do console.log()
+//console.log('Olá Mundo!')
+//console.log('primeiro')
+//console.log('segundo') 
 
-let year = 2022 - number
-console.log(year)
-console.log(number + year)
+//Declarando Variáveis
 
-console.log("Estou mexendo no console.log")
+//var - declara uma variavel de escopo global, quase não é usado
+//let - declara um variavel de escopo local, mais usado 
+//const - declara uma constante
 
-let quartaFeira = true
-console.log(quartaFeira)
-console.log(typeof quartaFeira)
+//let aniversario = 1998 // = siginifica recebe
+//console.log(aniversario)
+//console.log(variavel)
+
+
+//let indeficadorDaVariavel = atribuição
+
+//Tipos de dados
+
+//let nome = "Leandro" - string
+//let sobrenome = "Aragão" string
+//console.log(nome)
+
+//let templateString = `Oi eu sou ${nome} ${sobrenome}`
+//console.log(templateString)
+
+
+
+//let number = 10; //dados numeros em js não precisam ser especificados(int, float,negative)
+//console.log(number)
+
+//let bolaen  = true // dados boleanos, tipo verdadeiro ou falso
+//console.log(bolaen)
+
+//let type = null //dado do tipo nulo, inexistente 
+
+//let dado = undefined //esse dado é indefinido
+//console.log(dado)
+
+//let numero = NaN //dado não numerico
+
+//var _nome = 'robert'
+
+
+//usando aspas duplas e simples
+//console.log("oi 'to aqui'")
+//console.log('oi "to aqui"')
+
+//Redeclarando variaveis
+//let identifcadorDeNome;
+
+/* let nome = "leno"
+nome = "roberto"
+console.log(nome)
+
+*Declanrando uma variavel cujo valor inicial está undefined e verificando seu tipo apos a redeclaração
+let meuNome
+console.log(meuNome);
+meuNome = false
+console.log(typeof meuNome) */
