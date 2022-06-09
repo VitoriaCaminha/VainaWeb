690d47d02e0a2b8510391dc1cf8960d0ce80a0c4
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-08 21:07:55 -0300

    Challenge 01 - Data Structure

---
+++ module2/challenge1/script.js
@@ -0,0 +1,13 @@
+let name = "Vitória"
+
+let number = 23
+let year = 2022
+year = 2022 - 23
+
+console.log(number + year)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
