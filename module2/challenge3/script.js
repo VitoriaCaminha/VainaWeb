06756b8fac64fa1652ac3108702a3c7065cc2e8b
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-13 20:46:43 -0300

    Challenge 03 - for/while

---
+++ module2/challenge3/script.js
@@ -0,0 +1,20 @@
+let number = 10
+let numerate = 0
+
+for (number = 1; number <= 10; number++) {
+    console.log(number)
+}
+
+while (number > 1) {
+    number--
+    console.log(number)
+}
+
+for (let count = 1; count <= 100; count += 2) {
+    console.log(count)
+}
+
+while (numerate <= 100) {
+    console.log(numerate)
+    numerate += 2
+}
