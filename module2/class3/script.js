8b56360416db31e2e726580548fb33a0828b301d
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-13 10:33:49 -0300

    Third class of module 2

---
+++ module2/class3/script.js
@@ -0,0 +1,93 @@
+// let dado = 18    //entrada
+
+/*
+if(condição) {   //processamento (executar o bloco de código)
+    tarefa
+}
+*/
+
+// saída é a tarefa sendo realizada
+
+
+
+// Operadores de atribuição
+// = atribui um valor
+// + atribui uma concatenação (serve para somar também)
+// - atribui uma subtração
+// / atribui uma divisão
+// * atribui uma multiplicação
+
+/* 
+var x = 1
+var y = 2
+let b
+
+// soma
+
+console.log(x = x + y) // completo
+console.log(x += y) // simplificado
+
+console.log(b = x + y) // completo
+console.log(b)
+
+
+// incremento ++
+// decremento --
+
+console.log(x += y) // simplificado
+console.log(x -= y)
+*/
+
+
+
+/*
+// Estrutura do while
+
+variavel = valor
+
+while(condição) {
+    tarefa
+    iteração
+}
+
+// Uma declaração que inicia com a palavra chave while cria loop
+
+let contador = 0
+
+while(12 >= contador){
+    console.log(contador)/
+    contador+=2
+}
+*/
+
+/*
+// Estrutura do for
+
+for(variável contadora; condição; interação){
+    tarefa
+}
+
+        variável     condição     interação
+for (let count = 1; count <= 12; count += 2){
+    console.log(count) // tarefa
+}
+
+for (let count = 1; count <= 22; count++) {
+    console.log(count)
+}
+
+for (let count = 22; count >= 2; count--) {
+    console.log(count)
+}
+*/
+
+// let contador = 1
+
+// while (contador <= 100) {
+//     console.log(contador)
+//     contador += 2
+// }
+
+// for (contador = 1; contador <= 100; contador += 2) {
+//     console.log(contador)
+// }
