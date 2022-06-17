40280f1313119b7e6b44801eff8cf5f05bec3da1
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-17 10:07:23 -0300

    fifth class of module 2

---
+++ module2/class5/script.js
@@ -0,0 +1,30 @@
+// Revisão das aulas anteriores
+
+/*
+    = atribui valor ou verifica tipo
+    == verifica se os valores são iguais
+    === verifica se os valores e os tipos são iguais
+    != verifica se os valores são diferentes
+    !== verifica se os valores e os tipos são diferentes
+
+    < e > verificam se um valor numérico é maior ou menor que o outro
+    >= verifica se um número é menor ou igual a outro
+    <= verifica se um número é maior ou igual a outro
+
+    em valores binários onde 0 é false e 1 é true
+
+    string é tudo o que estiver entre '' ou ""
+    number não precisa de aspas
+    boolean verifica se o valor é true ou false
+    undefined é quando o valor não está definido
+    null é quando um valor é nulo
+    NaN é quando é esperado um número e o tipo da atribuição é diferente
+
+    template string serve para concatenar variáveis com textos, usando ${} e estando entre ``
+
+    var é de escopo global e pode ter seu valor alterado. Se não houver valor, será null
+    let é de escopo local de bloco, o valor pode ser alterado. Se não houver valor, será
+    const é de escopo global e seu valor não pode ser alterado
+*/
+
+// Posso mostrar várias variáveis no console.log usando template string
