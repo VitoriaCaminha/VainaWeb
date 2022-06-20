624a40416d9eb68e1dbe1093360bc9bcd11d31d3
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-20 15:52:34 -0300

    sixth class of module 2

---
+++ module2/class6/script.js
@@ -0,0 +1,132 @@
+// função é um bloco de instruções para criar funcionalidades do código
+
+// o () depois do nome da função é o parâmetro e dentro dele vem o valor do parâmetro
+
+// dentro das {}, depois do parâmetro, vem a tarefa
+
+/*
+function nome() {
+    console.log('Olá Aline')
+}
+
+nome()
+*/
+
+/*
+function nome(mundo) {
+    let sobre = '33'
+    console.log(sobre)
+}
+nome()
+*/
+
+/*
+function conta(x, y, z, b) {
+    return x**y + z / b
+}
+
+let result = conta(7, 2, 50, 3)
+console.log(result)
+*/
+
+/*
+function double(x) {
+    return x * 2
+}
+
+console.log(double(5))
+*/
+
+
+
+//Else if
+
+/*
+let dia = 3
+if(dia === 0){
+    console.log("domingo")
+}else if (dia === 1){
+    console.log("segunda")
+}else if (dia === 2){
+    console.log("terça")
+}else if (dia === 3){
+    console.log("Quarta")
+}else if (dia === 4){
+    console.log("Quinta")
+}else if (dia === 5){
+    console.log("Sexta")
+}else if (dia === 6){
+    console.log("Sabado")
+}else{
+    console.log("dia invalido")
+}
+*/
+
+
+
+//Estrutura Basica Switch
+
+/*
+    switch(expressão){
+    case valor1:  //Executa quando o resultado for igual a valor1;
+    break;
+    case valor2:  //Executa quando o resultado for igual a valor2;
+    break;
+    case valor3:  //Executa quando o resultado for igual a valor3;
+    break;
+    default: //Executa quando o valor é diferente de todos so cases;
+    break;
+    }
+*/
+
+/*
+let dia = 'kelly'
+
+switch(dia){
+    case 0: console.log('Domingo');
+    break;
+    case 1: console.log('Segunda');
+    break;
+    case 2: console.log('Terça');
+    break;
+    case 3: console.log('Quarta');
+    break;
+    case 4: console.log('Quinta');
+    break;
+    case 5: console.log('Sexta');
+    break;
+    case 6: console.log('Sabado');
+    break;
+    default: console.log('dia invalido');
+    break;
+    } 
+*/
+
+/*
+let food = "sopa"
+let aroma = "floral"
+
+switch(food){
+    case "lasanha": console.log("Não vendemos esta comida aqui")
+    break;
+    case "pizza": console.log("Estamos com escassez de pizza!")
+    break;
+    case "salada": console.log("Aqui está, são 3 reais.")
+    break;
+    default: console.log("Digite um opção disponivel.")
+    break;
+}
+
+switch(aroma){
+    case "amadeirado": console.log("Tem certeza que não prefere outro aroma?")
+    break;
+    case "citrico": console.log("Tem certeza que não prefere outro aroma?")
+    break;
+    case "frutal": console.log("Tem certeza que não prefere outro aroma?")
+    break;
+    case "floral": console.log("Compra efetuada com sucesso")
+    break;
+    default: console.log("Não trabalhamos com este tipo de perfume aqui.")
+    break;
+}
+*/
