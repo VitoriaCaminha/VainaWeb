23951c24875e58e7040164e5837460d7c9484997
VitoriaCaminha <vitoria@praciano.com.br>
2022-06-24 13:46:15 -0300

    eighth class of module 2

---
+++ module2/classes/class8/script.js
@@ -0,0 +1,119 @@
+// Objetos
+
+/*
+let variavel = 'David'
+
+const dados = {
+    nome: variavel,
+    idade: 19,
+    endereco: 'portugal',
+    dinheiro: 20.100,
+    amigos: ['João', 'paula', 'Vitória']
+}
+
+dados.comidaFavorita = 'carne assada' // Adiciona uma propriedade ao objeto
+delete dados.dinheiro // deleta uma propriedade do objeto
+
+const cadastros = [{
+},
+{
+}
+]
+*/
+
+
+
+// Revisão
+
+/*
+[] Array
+{ } Objeto
+
+let dados = ["João", "debora", "kelly"]
+console.log(dados)
+
+dados.unshift('Lindo') // Adiciona um valor no primeiro indice do array
+dados.pop() // Remove o ultimo valor do array
+dados.shift() // Remove o primeiro valor do array
+dados.push('sp') // Substitui o ultimo valor do array
+dados.splice(1, 2) // Remove os dois valores a partir do indice 1 do array
+dados.splice(1, 0, 'casa') // Substitui o valor do indice 1 do array
+*/
+
+
+/*
+const Paula = [{  // Array de objetos
+    nome: "stefany",
+    idade: 33,
+    cidade: "brasilia",
+},
+
+{
+    nome: "leandro",
+    idade: 23,
+    cidade: "rio",
+},
+
+{
+    nome: "kelly",
+    idade: 22,
+    cidade: "sp",
+},
+{
+    nome: "vitoria",
+    idade: 23,
+    cidade: "olinda",
+}]
+
+console.log(Paula[2].cidade)
+console.log(Paula[3].cidade)
+console.table(Paula)
+
+
+
+const cadastro = [{
+    nome: 'Aline',
+    idade: 30,
+    cidade: 'rj',
+    comidaFavorita: "pizza"
+},
+{
+    nome: 'Jordan',
+    idade: 22,
+    cidade: 'fortaleza'
+}
+]
+console.log(cadastro[1].nome)
+console.log(cadastro[0].comidaFavorita)
+
+
+
+const tabela = [{
+    nome: "david",
+    idade: 20,
+},
+{
+    nome: "kelly",
+    idade: 20
+}]
+
+console.log(tabela[1].nome)
+
+
+const cadastros = [{
+    nome: 'Leticia',
+    idade: 22,
+    cidade: 'Bertioga',
+},
+{
+    nome: 'Guto',
+    idade: 30,
+    cidade: 'Rio',
+},
+{
+    nome: 'João',
+    idade: 20,
+    cidade: 'São Paulo',
+},]
+console.log(cadastros[1].nome)
+*/
