b6aca2108d7396ed555d21f018244a6fe43b84f2
VitoriaCaminha <vitoria@praciano.com.br>
2022-07-06 19:51:13 -0300

    LOL

---
+++ module2/challenges/challenge7/script.js
@@ -0,0 +1,10 @@
+// button.addEventListener('click', function () {
+//     button.classList.remove('button')
+//   button.classList.add('box')
+// })
+
+function changeButton() {
+  let button = document.querySelector('.button')
+  button.classList.remove('button')
+  button.classList.add('box')
+}
