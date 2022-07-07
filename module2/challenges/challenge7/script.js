7ebc322adb087fcd92f933b66c8640154b0b8ab5
VitoriaCaminha <vitoria@praciano.com.br>
2022-07-06 21:03:28 -0300

    LOL

--- module2/challenges/challenge7/script.js
+++ module2/challenges/challenge7/script.js
@@ -1,10 +1,16 @@
-// button.addEventListener('click', function () {
-//     button.classList.remove('button')
-//   button.classList.add('box')
-// })
+const input1 = document.querySelector('.username')
+const input2 = document.querySelector('.password')
+const button = document.querySelector('.button')
 
-function changeButton() {
-  let button = document.querySelector('.button')
-  button.classList.remove('button')
-  button.classList.add('box')
+function handleChange() {
+  if (input1.value && input2.value.length >= 8) {
+    button.classList.remove('button')
+    button.classList.add('box')
+  } else {
+    button.classList.remove('box')
+    button.classList.add('button')
+  }
 }
+
+input1.addEventListener('input', handleChange)
+input2.addEventListener('input', handleChange)
