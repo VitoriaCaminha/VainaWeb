069cab47f8d1001f3cbed789ee5ce7465c9f8c13
VitoriaCaminha <vitoria@praciano.com.br>
2022-07-25 12:34:50 -0300

    monitoring class: responsive

--- module2/react/src/App.js
+++ module2/react/src/App.js
@@ -684,66 +684,82 @@ export default class Grupo3 extends Component {
 
 // Mentoria: responsividade
 
-// import React, { Component } from 'react'
-// import './App.css'
-
-// export default class Leandro extends Component {
-//   state = {
-//     nome: 'Restaurante',
-//     menu: ['inicio', 'sobre', 'contatos'],
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <header>
-//           <img
-//             className="logo"
-//             src="https://dnxstorage.blob.core.windows.net/gruporequinte/Blog/UrlFotoPequena3..png"
-//             alt="logo"
-//           />
-//           <nav>
-//             <ul>
-//               {this.state.menu.map((item, index) => (
-//                 <li key={index}>{item}</li>
-//               ))}
-//             </ul>
-//             <img
-//               className="menu"
-//               src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
-//               alt="menu"
-//             />
-//           </nav>
-//         </header>
-//         <main></main>
-//       </>
-//     )
-//   }
-// }
-
-// class 21: styled-components
-
 import React, { Component } from 'react'
-import styled from 'styled-components'
-import Main from './components/Main'
+import './App.css'
 
-const Container = styled.div`
-  border: solid;
-  background-color: gray;
-  display: flex;
-  justify-content: center;
-
-  h1 {
-    font-size: 40px;
+export default class Leandro extends Component {
+  state = {
+    nome: 'Restaurante',
+    menu: ['inicio', 'sobre', 'contatos'],
+    btn: '☰',
   }
-`
 
-export default class Calculadora extends Component {
+  Modal = () => {
+    const menuHamburguer = document.querySelector('button')
+    // console.log(this.state.menu)
+    if (menuHamburguer.innerText === '☰') {
+      this.setState({
+        // fazer abrir e fechar a ul com React
+        menu: ['inicio', 'sobre', 'contatos'],
+        btn: (menuHamburguer.innerText = 'x'),
+      })
+    } else {
+      this.setState({
+        menu: [],
+        btn: (menuHamburguer.innerText = '☰'),
+      })
+    }
+  }
+
   render() {
     return (
-      <Container>
-        <Main />
-      </Container>
+      <>
+        <header>
+          <img
+            className="logo"
+            src="https://dnxstorage.blob.core.windows.net/gruporequinte/Blog/UrlFotoPequena3..png"
+            alt="logo"
+          />
+          <nav>
+            <ul className="close">
+              {this.state.menu.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <button className="menu" onClick={this.Modal}>
+              {this.state.btn}
+            </button>
+          </nav>
+        </header>
+        <main></main>
+      </>
     )
   }
 }
+
+// class 21: styled-components
+
+// import React, { Component } from 'react'
+// import styled from 'styled-components'
+// import Main from './components/Main'
+
+// const Container = styled.div`
+//   border: solid;
+//   background-color: gray;
+//   display: flex;
+//   justify-content: center;
+
+//   h1 {
+//     font-size: 40px;
+//   }
+// `
+
+// export default class Calculadora extends Component {
+//   render() {
+//     return (
+//       <Container>
+//         <Main />
+//       </Container>
+//     )
+//   }
+// }
