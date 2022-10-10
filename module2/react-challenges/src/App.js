83b8d00e365abec027cde12cc73472b917dfc649
VitoriaCaminha <vitoria@praciano.com.br>
2022-10-10 16:59:25 -0300

    challenge 06: ToDo App

--- module2/react-challenges/src/App.js
+++ module2/react-challenges/src/App.js
@@ -1,17 +1,74 @@
+// Desafio 06: ToDo App
+
+import React, { useState } from "react";
+
+function App() {
+  const [tarefas, setTarefas] = useState("");
+  const [listadeTarefas, setLista] = useState([]);
+
+  console.log(listadeTarefas);
+
+  const nome = { tarefas: tarefas, id: Date.now() };
+
+  const handleClick = () => {
+    if (tarefas === "") {
+    } else {
+      setLista([...listadeTarefas, nome]);
+    }
+    setTarefas("");
+  };
+
+  const Remove = (id) => {
+    setLista(listadeTarefas.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+        <input
+          value={tarefas}
+          onChange={(e) => {
+            setTarefas(e.target.value);
+          }}
+        />
+        <button onClick={() => handleClick()}>ADD</button>
+      </form>
+      {listadeTarefas.map((item, index) => (
+        <div key={index}>
+          <ul>
+            <li>{item.tarefas}</li>
+          </ul>
+          <button
+            onClick={() => {
+              Remove(item.id);
+            }}>
+            Remove
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+
 // Desafio 05: Página filmes e séries
 
-import React, { Component } from "react";
-import Header from "./components/Header";
+// import React, { Component } from "react";
+// import Header from "./components/Header";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header/>
-      </div>
-    );
-  }
-}
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header/>
+//       </div>
+//     );
+//   }
+// }
 
 // Desafio 04: props
 
