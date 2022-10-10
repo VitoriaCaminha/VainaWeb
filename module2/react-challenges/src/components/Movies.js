990b72a981d8673796d09ad473f770af53beed52
VitoriaCaminha <vitoria@praciano.com.br>
2022-10-10 13:09:55 -0300

    fix: div estructure

--- module2/react-challenges/src/components/Movies.js
+++ module2/react-challenges/src/components/Movies.js
@@ -14,30 +14,43 @@ const GlobalStyle = createGlobalStyle`
 }
 `
 
+const Section = styled.section`
+display: flex;
+justify-content: space-evenly;
+width: 100%;
+flex-wrap: wrap;
+border-bottom: 1px solid;
+`
+
 const Div = styled.div`
-width: 30vw;
-height: 65vh;
-display: inline-block;
-flex-direction: column;
+width: 60%;
+display: flex;
+justify-content: center;
 align-items: center;
+flex-direction: column;
+figure {
+    width: 40%;
+    display: flex;
+    justify-content: center;
+}
 `
 
 const Img = styled.img`
-width: 40%;
+width: 50%;
 `
 
 const Ul = styled.ul`
 display: flex;
 flex-direction: column;
-justify-content: space-between;
+width: 50%;
 `
 
 const Title = styled.li`
-font-size: 1.5rem;
+font-size: 1rem;
 `
 
 const Li = styled.li`
-font-size: 0.7rem;
+font-size: 0.5rem;
 `
 
 const FilmesApi = axios.create({
@@ -95,14 +108,18 @@ export default class Movies extends Component {
                 <GlobalStyle />
                 <input onChange={this.handleChange} />
                 {this.state.FilterMovies.map((item, index) => (
-                    <Div key={index}>
-                        <Img src={item.image} alt='posters' />
-                        <Ul>
-                            <Title>{item.original_title}</Title>
-                            <Li>{item.overview}</Li>
-                            <Li>Note: {item.vote_average}</Li>
-                        </Ul>
-                    </Div>
+                    <Section>
+                        <Div key={index}>
+                            <figure>
+                                <Img src={item.image} alt='posters' />
+                            </figure>
+                            <Ul>
+                                <Title>{item.original_title}</Title>
+                                <Li>{item.overview}</Li>
+                                <Li>Note: {item.vote_average}</Li>
+                            </Ul>
+                        </Div>
+                    </Section>
                 ))}
 
             </>
